0be47e12a8a330d9dd48f2763d4329a29a6027e4
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-07-11 22:44:53 -0400

    Add ALVR and x264 dependencies as submodules
    Fixes #1.

--- vvv.sh
+++ vvv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-rsync -a . mini2:~/Documents/stereoscreenshots
+rsync -a --exclude .git --exclude third_party . mini2:~/Documents/stereoscreenshots
 ssh mini2 "cd ~/Documents/stereoscreenshots && bash build.sh"
